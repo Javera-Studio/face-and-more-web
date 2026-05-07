c02176c7e0a2f458e418c33c43a5a169e85533bd
Jagoda1986 <jagoda.okafor@smart-resolution.at>
2026-05-07 21:11:58 +0200

    Update Treatments.tsx

--- src/components/home/Treatments.tsx
+++ src/components/home/Treatments.tsx
@@ -5,7 +5,7 @@ import skinImg from "@/assets/treatment-skin.jpg";
 import antiagingImg from "@/assets/treatment-antiaging.jpg";
 import fruchtImg from "@/assets/treatment-fruchtsaeure.jpg";
 import needlingImg from "@/assets/treatment-microneedling.jpg";
-import facialImg from "@/assets/treatment-facial.jpg";
+import facialImg from "@/assets/gesichtsbehandlungl.jpg";
 import pmuImg from "@/assets/treatment-pmu.jpg";
 
 const treatments = [
